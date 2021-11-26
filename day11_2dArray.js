6a3089080de84ff5074a59d036fa6644dfd93e92
throne.yash@gmail.com <throne.yash@gmail.com>
2021-11-26 14:04:00 -0700

    Adding day11

---
+++ day11_2dArray.js
@@ -0,0 +1,102 @@
+// Objective
+// Today, we are building on our knowledge of arrays by adding another dimension. Check out the Tutorial tab for learning materials and an instructional video.
+
+// Context
+// Given a  2D Array, :
+
+// 1 1 1 0 0 0
+// 0 1 0 0 0 0
+// 1 1 1 0 0 0
+// 0 0 0 0 0 0
+// 0 0 0 0 0 0
+// 0 0 0 0 0 0
+// We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+// a b c
+//   d
+// e f g
+// There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
+
+// Task
+// Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+// Example
+
+// In the array shown above, the maximum hourglass sum is  for the hourglass in the top left corner.
+
+// Input Format
+
+// There are  lines of input, where each line contains  space-separated integers that describe the 2D Array .
+
+// Constraints
+
+// Output Format
+
+// Print the maximum hourglass sum in .
+
+// Sample Input
+
+// 1 1 1 0 0 0
+// 0 1 0 0 0 0
+// 1 1 1 0 0 0
+// 0 0 2 4 4 0
+// 0 0 0 2 0 0
+// 0 0 1 2 4 0
+// Sample Output
+
+// 19
+// Explanation
+
+//  contains the following hourglasses:
+
+// 1 1 1   1 1 0   1 0 0   0 0 0
+//   1       0       0       0
+// 1 1 1   1 1 0   1 0 0   0 0 0
+
+// 0 1 0   1 0 0   0 0 0   0 0 0
+//   1       1       0       0
+// 0 0 2   0 2 4   2 4 4   4 4 0
+
+// 1 1 1   1 1 0   1 0 0   0 0 0
+//   0       2       4       4
+// 0 0 0   0 0 2   0 2 0   2 0 0
+
+// 0 0 2   0 2 4   2 4 4   4 4 0
+//   0       0       2       0
+// 0 0 1   0 1 2   1 2 4   2 4 0
+// The hourglass with the maximum sum () is:
+
+// 2 4 4
+//   2
+// 1 2 4
+
+function hourglassSum(arr) {
+  let max = -Infinity
+  for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = 1; j < arr.length - 1; j++) {
+      let sum =
+        arr[i][j - 1] +
+        arr[i][j] +
+        arr[i][j + 1] +
+        arr[i + 1][j] +
+        arr[i + 2][j - 1] +
+        arr[i + 2][j] +
+        arr[i + 2][j + 1]
+      if (sum > max) {
+        max = sum
+      }
+    }
+  }
+  return max
+}
+
+console.log(
+  hourglassSum([
+    [1, 1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0],
+    [0, 0, 2, 4, 4, 0],
+    [0, 0, 0, 2, 0, 0],
+    [0, 0, 1, 2, 4, 0],
+  ])
+)
